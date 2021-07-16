2c8e0d6ea6f7c7e61838ca20fdce44a291534223
huangy17 <huangy17@miamioh.edu>
2021-07-16 21:12:33 +0800

    上传市场活动删除和编辑操作

--- crm/src/main/java/com/bjpowernode/crm/workbench/dao/ActivityDao.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/dao/ActivityDao.java
@@ -15,4 +15,10 @@ public interface ActivityDao {
     List<Activity> getActivityListByCondition(Map<String, Object> map);
 
     int getTotalByCondition(Map<String, Object> map);
+
+    int delete(String[] ids);
+
+    Activity getById(String id);
+
+    int update(Activity a);
 }
--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityController.java
@@ -37,11 +37,98 @@ public class ActivityController extends HttpServlet {
             save(request,response);
         }else if("/workbench/activity/pageList.do".equals(servletPath)){
             pageList(request,response);
-        }else if("/workbench/activity/xxx.do".equals(servletPath)){
+        }else if("/workbench/activity/delete.do".equals(servletPath)){
+            delete(request,response);
+        }else if("/workbench/activity/getUserListAndActivity.do".equals(servletPath)){
+            getUserListAndActivity(request,response);
+        } else if("/workbench/activity/update.do".equals(servletPath)){
+            update(request,response);
+        } else if("/workbench/activity/xxx.do".equals(servletPath)){
             //xxx(request,response);
-        }else if("/workbench/activity/xxx.do".equals(servletPath)){
+        } else if("/workbench/activity/xxx.do".equals(servletPath)){
+            //xxx(request,response);
+        } else if("/workbench/activity/xxx.do".equals(servletPath)){
             //xxx(request,response);
         }
+    }
+
+    private void update(HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println("执行市场活动修改操作");
+
+
+        String id = request.getParameter("id");
+        String owner = request.getParameter("owner");
+        String name = request.getParameter("name");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String cost = request.getParameter("cost");
+        String description = request.getParameter("description");
+        //修改时间：当前系统时间
+        String editTime = DateTimeUtil.getSysTime();
+        //修改人：当前登录用户
+        String editBy = ((User)request.getSession().getAttribute("user")).getName();
+
+        Activity a = new Activity();
+        a.setId(id);
+        a.setCost(cost);
+        a.setStartDate(startDate);
+        a.setOwner(owner);
+        a.setName(name);
+        a.setEndDate(endDate);
+        a.setDescription(description);
+        a.setEditTime(editTime);
+        a.setEditBy(editBy);
+
+        ActivityService as = (ActivityService) ServiceFactory.getService(new ActivityServiceImpl());
+
+        boolean flag = as.update(a);
+
+        PrintJson.printJsonFlag(response,flag);
+
+
+
+
+    }
+
+    private void getUserListAndActivity(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("进入到查询用户信息列表和根据市场活动id查询单条记录的操作");
+
+        String id = request.getParameter("id");
+
+        ActivityService as = (ActivityService) ServiceFactory.getService(new ActivityServiceImpl());
+
+        /*
+
+            总结：
+                controller调用service的方法，返回值应该是什么
+                你得想一想前端要什么，就要从service层取什么
+
+            前端需要的，管业务层去要
+            uList
+            a
+
+            以上两项信息，复用率不高，我们选择使用map打包这两项信息即可
+            map
+
+         */
+        Map<String,Object> map = as.getUserListAndActivity(id);
+
+        PrintJson.printJsonObj(response, map);
+
+    }
+
+    private void delete(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("执行市场活动的删除操作");
+
+        String ids[] = request.getParameterValues("id");
+
+        ActivityService as = (ActivityService) ServiceFactory.getService(new ActivityServiceImpl());
+
+        boolean flag = as.delete(ids);
+
+        PrintJson.printJsonFlag(response,flag);
+
     }
 
     private void pageList(HttpServletRequest request, HttpServletResponse response) {
